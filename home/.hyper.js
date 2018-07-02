bcb274c53fdf855152bd3f1369b025365df90527
Strajk <strajk@me.com>
2018-07-02 16:57:46 +0200

    Hyper config

---
+++ home/.hyper.js
@@ -0,0 +1,27 @@
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 12,
+    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+
+    cursorShape: 'BEAM',
+    cursorBlink: true,
+    padding: '8px 10px',
+    shellArgs: ['--login'],
+    env: {},
+    bell: 'SOUND',
+    copyOnSelect: false
+  },
+
+  plugins: [
+    'hypercwd',
+    'hyperterm-1password',
+    'hyperterm-paste',
+    'hyperterm-tabs',
+    'hyperlinks',
+    'hyper-search',
+    'hyper-snazzy'
+  ],
+
+  keymaps: {}
+};
