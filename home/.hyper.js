e60ba3dc29bec7901ae61164ae335c82d0dbeefb
Strajk <strajk@me.com>
2019-06-09 18:08:27 +0200

    Hack font for Hyper

--- home/.hyper.js
+++ home/.hyper.js
@@ -2,7 +2,7 @@ module.exports = {
   config: {
     updateChannel: 'stable',
     fontSize: 12,
-    fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'Hack, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     cursorShape: 'BEAM',
     cursorBlink: true,
